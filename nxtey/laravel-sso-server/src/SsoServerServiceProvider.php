c4be1346c49cb467fd0cae460069bca3814be5bb
aklegend <64873710+aklegend@users.noreply.github.com>
2026-08-19 23:22:41 +0300

    Enhance SsoServerServiceProvider with view support
    Added view publishing and loading functionality.

--- nxtey/laravel-sso-server/src/SsoServerServiceProvider.php
+++ nxtey/laravel-sso-server/src/SsoServerServiceProvider.php
@@ -24,7 +24,15 @@ class SsoServerServiceProvider extends ServiceProvider
             $this->publishes([
                 __DIR__ . '/../database/migrations/' => database_path('migrations'),
             ], 'sso-server-migrations');
+
+            // Publish views
+            $this->publishes([
+                __DIR__ . '/../resources/views' => resource_path('views/vendor/sso-server'),
+            ], 'sso-server-views');
         }
+
+        // Load views from the package
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'sso-server');
 
         // Load routes
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
